10a5f1d5696243536b4117b6f59131ed608ecb72
sekhaoula <se.khaoula@icloud.com>
2023-03-16 14:40:13 -0700

    update the file 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
- * main - prints a quote
+ * main - Entry point
  *
- * Returne : always 0 (success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0) ;
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
+
