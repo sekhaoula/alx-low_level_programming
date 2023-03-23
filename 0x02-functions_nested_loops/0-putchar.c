bc1c0a6678e032dec9122c56cc21d06e76484998
sekhaoula <se.khaoula@icloud.com>
2023-03-22 19:28:10 -0700

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,7 +9,6 @@
  */
 int main(void)
 {
-<<<<<<< HEAD
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -21,17 +20,4 @@ int main(void)
 	_putchar('\n');
 
 	return (0);
-=======
-        _putchar('_');
-        _putchar('p');
-        _putchar('u');
-        _putchar('t');
-        _putchar('c');
-        _putchar('h');
-        _putchar('a');
-        _putchar('r');
-        _putchar('\n');
-
-        return (0);
->>>>>>> a027d3abaa45fa4bcd6ca69e29454d2e2833bb55
 } 
