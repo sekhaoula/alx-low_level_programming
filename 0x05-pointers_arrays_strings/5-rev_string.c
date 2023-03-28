05014cc9d08e9e6ab7e38517c09d44d517093ba3
sekhaoula <se.khaoula@icloud.com>
2023-03-28 14:20:56 -0700

    files for tsk 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,7 +8,7 @@
 void rev_string(char *s)
 {
 	char str;
-	int i, len, len1;
+	int i,len,len1;
 	
 	len = 0;
 	len1 = 0;
@@ -19,7 +19,7 @@ void rev_string(char *s)
 	}
 	
 	len1 = len - 1;
-	
+
 	for (i = 0; i < len / 2; i++)
 	{
 		str = s[i];
