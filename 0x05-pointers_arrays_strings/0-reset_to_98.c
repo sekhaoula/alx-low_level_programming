12499f2629ce5ecbe8b9ddfafc58e6a610f8ea20
sekhaoula <se.khaoula@icloud.com>
2023-03-27 18:34:39 -0700

    update

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -6,8 +6,8 @@
  * Return: Always 0;
 */
 
-void reset_to_98(int *n) //pointer to an int//
+void reset_to_98(int *n) /*pointer to an int*/
 {
-	*n = 98; // n will be 98 instead of 402 //
+	*n = 98; /* n will be 98 instead of 402*/ 
 }
 
