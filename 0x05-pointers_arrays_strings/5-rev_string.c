895eed5ec6da46aa3cdf078aece0bd8c4b5430a8
sekhaoula <se.khaoula@icloud.com>
2023-03-28 14:10:53 -0700

    files for tsk 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+/**
+ *rev_string - unction that reverses a string. 
+ * @s: the string to revers
+*/
+
+void rev_string(char *s)
+{
+	char str;
+	int i, len, len1;
+	
+	len = 0;
+	len1 = 0;
+	
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	
+	len1 = len - 1;
+	
+	for (i = 0; i < len / 2; i++)
+	{
+		str = s[i];
+		s[i] = s[len1];
+		s[len1--] = str;
+	}
+}
+
