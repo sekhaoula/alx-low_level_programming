e3080751627a3cfbcc37cb69721204968a2c004d
sekhaoula <se.khaoula@icloud.com>
2023-03-27 18:53:00 -0700

    add files of task 2

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ *  swap_int - swaps the value of the variable
+ * the pointer will swap a by b
+ * @a: integer to swap
+ * @b: integer swaped
+*/
+
+void swap_int(int *a, int *b)
+{
+    *a = 42;
+    *b = 98;
+}
+
