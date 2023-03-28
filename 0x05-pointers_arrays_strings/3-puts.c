f790bc5b03899fd2b56011c3a735fc8183a2a4e6
sekhaoula <se.khaoula@icloud.com>
2023-03-27 20:54:17 -0700

    add files of task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * _puts - prints a string followed by a new line
+ * @str: the string printed
+ * Returns: Always 0
+*/
+
+void _puts(char *str)
+{
+	int i = 0;
+	while(str[i] != '\0')
+	{
+		putchar(str[i]);
+		i++;
+	}
+	putchar('\n');
+}
+
