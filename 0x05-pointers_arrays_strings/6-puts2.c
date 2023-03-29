9f6af5a35b3fe88be7c27c494b304108049e45b9
sekhaoula <se.khaoula@icloud.com>
2023-03-28 17:04:39 -0700

    add files for task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: the string to print
+ */
+
+void puts2(char *str)
+{
+	int N, i = 0;
+
+	while (str[i] !='\0')
+	{
+		i++;
+	}
+
+	for (N = 0; N < i; N+=2)
+	{
+		putchar(str[N]);
+	}
+
+	putchar('\n');
+
+}
+	
