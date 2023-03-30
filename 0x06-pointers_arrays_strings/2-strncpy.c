a821b4ad11dea51e0e76d0305a093ceb59374879
sekhaoula <se.khaoula@icloud.com>
2023-03-30 04:17:56 -0700

    update

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,18 +11,17 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
+	int index = 0, src_len = 0;
 
-	while (dest[i] != '\0' && i < n)
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	while (i < n)
-	{
-		dest[i] = '\0';
-		i++;
-	}
+	while (src[index++])
+		src_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
+
+	for (index = src_len; index < n; index++)
+		dest[index] = '\0';
+
 	return (dest);
 }
 
